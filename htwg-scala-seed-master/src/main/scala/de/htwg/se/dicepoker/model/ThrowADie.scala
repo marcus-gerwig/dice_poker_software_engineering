454540b3ea12ff24563194b0e2627a56fafbe6de
MarcusG-PC\Marcus <marcus.gerwig@htwg-konstanz.de>
2017-04-06 18:34:45 +0200

    Corrected random function to throw a die

--- htwg-scala-seed-master/src/main/scala/de/htwg/se/dicepoker/model/ThrowADie.scala
+++ htwg-scala-seed-master/src/main/scala/de/htwg/se/dicepoker/model/ThrowADie.scala
@@ -2,5 +2,5 @@ package de.htwg.se.dicepoker.model
 import java.util.Random
 
 object ThrowADie {
-  def throwDie = new Random().nextInt((6 - 1 + 1) + 1)
+  def throwDie = new Random().nextInt(6 - 1 + 1) + 1
 }
