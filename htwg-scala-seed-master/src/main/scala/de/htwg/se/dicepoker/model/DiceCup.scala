a68f43d36d290351319b863b52b20bd43abdc7ea
MarcusG-PC\Marcus <marcus.gerwig@htwg-konstanz.de>
2017-04-06 17:04:38 +0200

    removed comment in DiceCup

--- htwg-scala-seed-master/src/main/scala/de/htwg/se/dicepoker/model/DiceCup.scala
+++ htwg-scala-seed-master/src/main/scala/de/htwg/se/dicepoker/model/DiceCup.scala
@@ -1,6 +1,6 @@
 package de.htwg.se.dicepoker.model
 
-//removed numberOfDice:Int
+
 case class DiceCup(actualDice: Array[Die], highestDieCombination: Array[Die]) {
   def this(actualDice: Array[Die]) {
     this(actualDice, null)
