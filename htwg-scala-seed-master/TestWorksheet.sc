c4951df1d097353e93abf8800c667a0350c66f3f
Andreas Graule <andreas_graule@hotmail.de>
2017-04-21 15:11:57 +0200

    Function getMaxResult added (not finished)

---
+++ htwg-scala-seed-master/TestWorksheet.sc
@@ -0,0 +1,7 @@
+object TestWorksheet {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+  
+  def f(x: Int): Int = x + 1                      //> f: (x: Int)Int
+  
+  
+}
--- htwg-scala-seed-master/src/main/scala/de/htwg/se/dicepoker/model/DiceCup.scala
+++ htwg-scala-seed-master/src/main/scala/de/htwg/se/dicepoker/model/DiceCup.scala
@@ -5,4 +5,31 @@ case class DiceCup(dieCombi: List[Int] = Nil) {
 
   def roll(dice: Int): DiceCup = copy((1 to dice).toList.map(x => scala.util.Random.nextInt(6) + 1))
   def countTuples = dieCombi.groupBy(l => l).map(t=>(t._1,t._2.length))
+  def getMaxResult ={
+     val tuples = this.countTuples
+     var maxFrequency = 0
+     var respDieValue = 1
+     
+     respDieValue = tuples.keys.max
+     
+     
+     maxFrequency=tuples.values.max 
+     
+     if (maxFrequency == 1) respDieValue
+     
+     else (
+     
+     
+     )
+         
+     
+     for ((key, value)<-tuples){
+       
+       
+     }
+         
+       
+    
+    
+  }
 }
