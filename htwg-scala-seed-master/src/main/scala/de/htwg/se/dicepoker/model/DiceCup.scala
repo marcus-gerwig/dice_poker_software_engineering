1ef895b8e6cfa944525ebc6354173a749cc6a279
MarcusG-PC\Marcus <marcus.gerwig@htwg-konstanz.de>
2017-04-21 16:48:21 +0200

    finished getMaxResult:(Int,Int)

--- htwg-scala-seed-master/src/main/scala/de/htwg/se/dicepoker/model/DiceCup.scala
+++ htwg-scala-seed-master/src/main/scala/de/htwg/se/dicepoker/model/DiceCup.scala
@@ -4,32 +4,25 @@ import scala.util.Random
 case class DiceCup(dieCombi: List[Int] = Nil) {
 
   def roll(dice: Int): DiceCup = copy((1 to dice).toList.map(x => scala.util.Random.nextInt(6) + 1))
-  def countTuples = dieCombi.groupBy(l => l).map(t=>(t._1,t._2.length))
-  def getMaxResult ={
-     val tuples = this.countTuples
-     var maxFrequency = 0
-     var respDieValue = 1
-     
-     respDieValue = tuples.keys.max
-     
-     
-     maxFrequency=tuples.values.max 
-     
-     if (maxFrequency == 1) respDieValue
-     
-     else (
-     
-     
-     )
-         
-     
-     for ((key, value)<-tuples){
-       
-       
-     }
-         
-       
-    
-    
+  def countTuples(list: List[Int]) = list.groupBy(l => l).map(t => (t._1, t._2.length))
+  def cupHasTuples(tuples: Map[Int, Int]): Boolean = if (tuples.values.max > 1) true else false
+  
+  def getMaxResult(tuples: Map[Int, Int]): (Int, Int) = {
+    var maxFrequency: Int = 0
+    val highDie = tuples.keys.max
+    if (cupHasTuples(tuples)) {
+      var key = tuples.head._1
+      var value = tuples.head._2
+
+      for ((x, y) <- tuples) {
+        if ((y >= value && x > key) || (y > value)) {
+          value = y
+          key = x
+        }
+      }
+      (key, value)
+    } else (highDie, 1)
+
   }
+
 }
