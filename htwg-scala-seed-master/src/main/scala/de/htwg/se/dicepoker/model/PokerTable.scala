83a975ca730ca1bfbe3fffd36616802c43f55489
MarcusG-PC\Marcus <marcus.gerwig@htwg-konstanz.de>
2017-05-01 17:03:38 +0200

    Added simple functions to PokerTable

--- htwg-scala-seed-master/src/main/scala/de/htwg/se/dicepoker/model/PokerTable.scala
+++ htwg-scala-seed-master/src/main/scala/de/htwg/se/dicepoker/model/PokerTable.scala
@@ -1,5 +1,6 @@
 package de.htwg.se.dicepoker.model
 
 case class PokerTable (players: List[Player]) {
-  
+  def addPlayer(newPlayer : Player) = copy(newPlayer :: players)
+  def size = players.size
 }
