25c5ca0e26d8305518ab0d2fffb088ca6d62089c
MarcusG-PC\Marcus <marcus.gerwig@htwg-konstanz.de>
2017-05-05 09:14:37 +0200

    Changed return type of method getMaxResult from (Int,Int) to Result

--- htwg-scala-seed-master/src/main/scala/de/htwg/se/dicepoker/model/DiceCup.scala
+++ htwg-scala-seed-master/src/main/scala/de/htwg/se/dicepoker/model/DiceCup.scala
@@ -2,12 +2,12 @@ package de.htwg.se.dicepoker.model
 import scala.util.Random
 
 case class DiceCup(dieCombi: List[Int] = Nil) {
-  
+
   def roll(numDice: Int): DiceCup = copy((1 to numDice).toList.map(x => scala.util.Random.nextInt(6) + 1))
-  def countTuples(list: List[Int]) = list.groupBy(l => l).map(t => (t._1, t._2.length))
+  def countTuples(list: List[Int]): Map[Int, Int] = list.groupBy(l => l).map(t => (t._1, t._2.length))
   def cupHasTuples(tuples: Map[Int, Int]): Boolean = if (tuples.values.max > 1) true else false
 
-  def getMaxResult(tuples: Map[Int, Int]): (Int, Int) = {
+  def getMaxResult(tuples: Map[Int, Int]): Result = {
     var maxFrequency: Int = 0
     val highDie = tuples.keys.max
     if (cupHasTuples(tuples)) {
@@ -20,8 +20,8 @@ case class DiceCup(dieCombi: List[Int] = Nil) {
           key = x
         }
       }
-      (key, value)
-    } else (highDie, 1)
+      Result(key, value)
+    } else Result(highDie, 1)
 
   }
 
