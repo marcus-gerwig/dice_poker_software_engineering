16ccf3805c101d0cdf0d7a9b5ebb628e9bb502da
MarcusG-PC\Marcus <marcus.gerwig@htwg-konstanz.de>
2017-05-05 09:15:19 +0200

    Added a property DiceCup to Player class

--- htwg-scala-seed-master/src/main/scala/de/htwg/se/dicepoker/model/Player.scala
+++ htwg-scala-seed-master/src/main/scala/de/htwg/se/dicepoker/model/Player.scala
@@ -1,7 +1,8 @@
 package de.htwg.se.dicepoker.model
 
-case class Player(name: String, playerResult: Result, diceCount: Int = 5) {
-  def this(name: String, diceCount: Int) = this(name, Result(1, 1), diceCount)
+case class Player(name: String, playerResult: Result, diceCount: Int = 5, diceCup: DiceCup) {
+  def this(name: String, diceCount: Int) = this(name, Result(1, 1), diceCount, DiceCup(Nil))
   def hasLostRound: Player = copy(name, playerResult, diceCount - 1)
   def hasLostGame: Boolean = if (diceCount == 0) true else false
+  def setBid(dieValue: Int, freq: Int): Bid = new Bid(Result(dieValue, freq), this)
 }
