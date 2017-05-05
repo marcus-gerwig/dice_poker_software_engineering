26cefe18db720644e0e13aee09807c1fafdb2229
MarcusG-PC\Marcus <marcus.gerwig@htwg-konstanz.de>
2017-05-05 11:58:45 +0200

    Inserted AppConst.NUMBER_OF_DICE into constructors

--- htwg-scala-seed-master/src/main/scala/de/htwg/se/dicepoker/model/Player.scala
+++ htwg-scala-seed-master/src/main/scala/de/htwg/se/dicepoker/model/Player.scala
@@ -1,7 +1,11 @@
 package de.htwg.se.dicepoker.model
 
-case class Player(name: String, playerResult: Result, diceCount: Int = 5, diceCup: DiceCup) {
+import de.htwg.se.dicepoker.util.AppConst
+
+case class Player(name: String, playerResult: Result, diceCount: Int = AppConst.NUMBER_OF_DICE, diceCup: DiceCup) {
+  
   def this(name: String, diceCount: Int) = this(name, Result(1, 1), diceCount, DiceCup(Nil))
+  def this(name: String) = this(name, Result(1, 1), AppConst.NUMBER_OF_DICE, DiceCup(Nil))
   def hasLostRound: Player = copy(name, playerResult, diceCount - 1)
   def hasLostGame: Boolean = if (diceCount == 0) true else false
   def setBid(dieValue: Int, freq: Int): Bid = new Bid(Result(dieValue, freq), this)
