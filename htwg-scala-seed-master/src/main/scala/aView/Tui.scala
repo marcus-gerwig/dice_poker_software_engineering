87c5d3917eed5365ec8e7034f602c2b246dde26e
MarcusG-PC\Marcus <marcus.gerwig@htwg-konstanz.de>
2017-05-05 09:13:41 +0200

    Insert code from Mr Bogers Sudoko project

--- htwg-scala-seed-master/src/main/scala/aView/Tui.scala
+++ htwg-scala-seed-master/src/main/scala/aView/Tui.scala
@@ -3,5 +3,19 @@ package aView
 import de.htwg.se.dicepoker.controller.DPController
 
 class Tui(controller: DPController) {
-  def processInputLine(input: String): Unit = {}
+  def processInputLine(input: String): Unit = {
+//    input match {
+//      case "q" =>
+//      case "n"=> DPController.createEmptyGrid(size)
+//      case "r" => DPCcontroller.createRandomGrid(size, randomCells)
+//      case "s" =>
+//        val success= controller.solve
+//        if (success) println("Puzzle solved")else println("This puzzle could not be solved!")
+//      case _ => input.toList.filter(c => c != ' ').map(c => c.toString.toInt) match {
+//          case row :: column :: value :: Nil => controller.set(row, column, value)
+//          case _ =>
+//        }
+//
+//    }
+  }
 }
