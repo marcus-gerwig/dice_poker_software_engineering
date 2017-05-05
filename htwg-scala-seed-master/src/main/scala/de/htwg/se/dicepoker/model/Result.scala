1c13d9a638749bd98377842c0807bdc0ec8abb26
MarcusG-PC\Marcus <marcus.gerwig@htwg-konstanz.de>
2017-05-05 09:15:52 +0200

    Added method isHigherThan

--- htwg-scala-seed-master/src/main/scala/de/htwg/se/dicepoker/model/Result.scala
+++ htwg-scala-seed-master/src/main/scala/de/htwg/se/dicepoker/model/Result.scala
@@ -4,10 +4,17 @@ case class Result(dieValue: Int, frequency: Int) {
   def saveResult(value: Int, freq: Int): Result = copy(value, freq)
 
   override def toString: String = {
-    var result = ""
-    for (i <- 0 until frequency) {
+    var result = "" + dieValue
+    for (i <- 0 until frequency - 1) {
       result += " " + dieValue
     }
     return result
   }
+  
+  def isHigherThan(otherResult: Result) = {
+    if (this.dieValue > otherResult.dieValue) true
+    else if(this.dieValue == otherResult.dieValue && this.frequency > otherResult.frequency) true
+    else false
+  }
+  
 }
