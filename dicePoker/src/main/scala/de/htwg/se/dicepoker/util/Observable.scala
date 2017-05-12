1fd46f23f66c4846c9865736c9eb0f9c82fbdbf4
Marcus Gerwig <marcus.gerwig@htwg-konstanz.de>
2017-05-12 13:07:46 +0200

    approach to update observer by Event

--- dicePoker/src/main/scala/de/htwg/se/dicepoker/util/Observable.scala
+++ dicePoker/src/main/scala/de/htwg/se/dicepoker/util/Observable.scala
@@ -1,12 +1,16 @@
 package de.htwg.se.dicepoker.util
 
 trait Observer {
-  def update:Unit
+  def update(e: Event):Unit
 }
+
+abstract class Event
+object DiceWereRollen extends Event
+object PlayerHasWon extends Event
 
 class Observable {
   var subscribers: Vector[Observer] = Vector()
   def add(s: Observer):Unit = subscribers = subscribers :+ s
   def remove(s: Observer):Unit = subscribers = subscribers.filterNot(o => o == s)
-  def notifyObservers:Unit = subscribers.foreach(o => o.update)
+  def notifyObservers(e: Event):Unit = subscribers.foreach(o => o.update(e))
 }
