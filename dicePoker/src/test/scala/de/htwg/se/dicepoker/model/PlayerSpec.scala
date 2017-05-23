021a73bc182c07630a050f8ac6c8e6e4cb24db7e
Andreas Graule <andreas_graule@hotmail.de>
2017-05-24 00:13:33 +0200

    Tests written for model classes

--- dicePoker/src/test/scala/de/htwg/se/dicepoker/model/PlayerSpec.scala
+++ dicePoker/src/test/scala/de/htwg/se/dicepoker/model/PlayerSpec.scala
@@ -4,6 +4,26 @@ import org.scalatest.WordSpec
 import org.scalatest.Matchers
 
 class PlayerSpec extends WordSpec with Matchers{
+  "A Player" can{
+    
+    val aDiceCup = DiceCup(List(1,2,3,4,5))
+    var aPlayer = new Player("Testplayer", 5, aDiceCup) 
+    
+    "lose Round" in{
+      
+      aPlayer.hasLostRound() should be (new Player("Testplayer", 4, aDiceCup));
+    }
+    "lose Game" in {
+            
+      aPlayer.hasLostGame() should be (false);
+    }
+    
+    "is bidding" in {
+      
+      aPlayer.setBid(5, 2) should be (Bid(new Result (5,2),aPlayer))
+    }
+      
+    }
   
   
   
