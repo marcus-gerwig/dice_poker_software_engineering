e53fe8d90d6d0978588b0e37095003aab177caa4
Andreas Graule <andreas_graule@hotmail.de>
2017-06-27 20:19:26 +0200

    Added LOG-File Output (Name of Log-file: all.log)

--- dicePoker/src/main/scala/de/htwg/se/dicepoker/aview/Tui.scala
+++ dicePoker/src/main/scala/de/htwg/se/dicepoker/aview/Tui.scala
@@ -30,34 +30,34 @@ class Tui(controller: DPController) extends Observer {
       case WelcomeMsg => logger.info("Welcome to €€ DICE POKER €€!")
       case EnterPlayerName => {
         val index: Int = EnterPlayerName.attachment.asInstanceOf[Int]
-        println("Hello Player " + index + EOL + "Please enter your name:")
+        logger.info("Hello Player " + index + EOL + "Please enter your name:")
         val name = readLine
         controller.setPlayerName(index, name)
         println()
       }
-      case LetShowBegin => println("Alright, let the show begin...")
-      case ExplainCommands => println(tui + "start game: 's' | exit game: 'q' | restart: 'r'")
+      case LetShowBegin => logger.info("Alright, let the show begin...")
+      case ExplainCommands => logger.info(tui + "start game: 's' | exit game: 'q' | restart: 'r'")
       case DiceWereRollen => {
-        println(EOL + controller.printTable)
+        logger.info(EOL + controller.printTable)
         controller.beginRound
       }
       case PlayerHasWonRound => {
         val winner = controller.playerName(PlayerHasWonRound.attachment.asInstanceOf[Player])
-        println(tui + winner + " has won this round!" + EOL + "_________________________________________")
+        logger.info(tui + winner + " has won this round!" + EOL + "_________________________________________")
         controller.newRound
       }
-      case PlayerWithHighestBidLied => println(tui + controller.playerName(controller.getHighestBidPlayer) + " lied. His actual result was " + controller.playerResult(controller.getHighestBidPlayer) + ".")
+      case PlayerWithHighestBidLied => logger.info(tui + controller.playerName(controller.getHighestBidPlayer) + " lied. His actual result was " + controller.playerResult(controller.getHighestBidPlayer) + ".")
       case PlayerWithHighestBidNotLied => {
         val winner = controller.whichPlayerFollows(controller.getLastLoser.get)
-        println(tui + controller.playerName(winner.get) + " did not lie. His actual result was " + controller.playerResult(winner.get) + ".")
+        logger.info(tui + controller.playerName(winner.get) + " did not lie. His actual result was " + controller.playerResult(winner.get) + ".")
       }
-      case NewRound => println(tui + "New Round")
+      case NewRound => logger.info(tui + "New Round")
       case DeclareFirstBid => {
-        println(tui + controller.playerName(controller.getPlayerStarted.get) + ", please declare the first bid (e.g. 3,2 means your bid is a double of 3):")
+        logger.info(tui + controller.playerName(controller.getPlayerStarted.get) + ", please declare the first bid (e.g. 3,2 means your bid is a double of 3):")
         val input = readLine
         controller.declareFirstBid(input)
       }
-      case LineSeparator => println("->->->->->->->->->->->->")
+      case LineSeparator => logger.info("->->->->->->->->->->->->")
       case Input => {
         val input = readLine
         input match {
@@ -69,11 +69,11 @@ class Tui(controller: DPController) extends Observer {
       case AskIfMistrusts => {
         val playerHigh = controller.getHighestBidPlayer
         val playerFoll = controller.whichPlayerFollows(controller.getHighestBidPlayer)
-        println()
-        println(tui + "Highest bid at the moment = " + controller.getHighestBidResult)
-        println(tui + "Now it's your turn " + controller.playerName(playerFoll.get))
-        println(tui + "Do you mistrust " + controller.playerName(playerHigh) + " or do you want to set a higher bid?")
-        println(tui + "mistrust: 'm' | setHigherBid: 'b'")
+        logger.info()
+        logger.info(tui + "Highest bid at the moment = " + controller.getHighestBidResult)
+        logger.info(tui + "Now it's your turn " + controller.playerName(playerFoll.get))
+        logger.info(tui + "Do you mistrust " + controller.playerName(playerHigh) + " or do you want to set a higher bid?")
+        logger.info(tui + "mistrust: 'm' | setHigherBid: 'b'")
         val input = readLine
         input match {
           case "m" => controller.playerMistrusts
@@ -82,21 +82,21 @@ class Tui(controller: DPController) extends Observer {
       }
       case PrintPlayer => {
         val player: Player = PrintPlayer.attachment.asInstanceOf[Player]
-        println(controller.printPlayer(player))
+        logger.info(controller.printPlayer(player))
       }
       case RequestHigherBid => {
         val player: Player = RequestHigherBid.attachment.asInstanceOf[Player]
-        println(tui + controller.playerName(player) + ", please declare a higher bid than " + controller.getHighestBidResult + " :")
+        logger.info(tui + controller.playerName(player) + ", please declare a higher bid than " + controller.getHighestBidResult + " :")
         val input = readLine
         controller.declareHigherBid(input, player)
       }
       case GameIsOver => {
         val winner = controller.playerName(GameIsOver.attachment.asInstanceOf[Player])
-        println(tui + "...and the winner is " + winner + "!")
-        println(tui + "Congratulations, " + winner + "!")
+        logger.info(tui + "...and the winner is " + winner + "!")
+        logger.info(tui + "Congratulations, " + winner + "!")
       }
       case GameWasCancelled => {
-        println(tui + "The game is over. See you soon!")
+        logger.info(tui + "The game is over. See you soon!")
 
       }
     }
